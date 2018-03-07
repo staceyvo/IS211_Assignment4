76dcc53fb29774b850a68a822c205eeb3d173962
staceyvo <staceyvo@gmail.com>
2018-03-06 23:21:42 -0500

    udated binary_search_iterative

--- search_compare.py
+++ search_compare.py
@@ -17,8 +17,23 @@ def ordered_sequential_search(search_list):
     return None
 
 
-def binary_search_iterative(search_list):
-    pass
+def binary_search_iterative(search_list, elem):
+    left_bound = 0
+    right_bound = len(search_list) - 1
+    mid = (left_bound + right_bound) / 2
+
+
+    while left_bound != right_bound or search_list[left_bound] == elem or search_list[right_bound] == elem:
+        if elem == search_list[mid]:
+            return mid
+        elif elem > search_list[mid]:
+            left_bound = mid + 1
+        else:
+            right_bound = mid - 1
+        mid = (left_bound + right_bound) / 2
+    return None
+
+
 
 
 def binary_search_recursive(search_list):
@@ -26,49 +41,50 @@ def binary_search_recursive(search_list):
 
 
 if __name__ == '__main__':
-    lists500 = []
-    lists1000 = []
-    lists10000 = []
-    list_of_lists = [
-        lists500,
-        lists1000,
-        lists10000
-    ]
-
-    for _ in range(100):
-        lists500.append([random.randint(0, 1285398243) for _b in range(500)])
-
-        lists1000.append([random.randint(0, 1285398243) for _b in range(500)])
-
-        lists10000.append([random.randint(0, 1285398243) for _b in range(500)])
-
-    for mega_list in list_of_lists:
-        search_time = datetime.timedelta(0, 0, 0)
-        for item in mega_list:
-            start = dt.now()
-            sequential_search(item)
-            search_time += dt.now() - start
-
-        print(
-        '{}, {} from {}'.format(sequential_search.__name__, search_time.total_seconds() / float(len(mega_list)), len(mega_list)))
-
-    list_of_functions = [
-        ordered_sequential_search,
-        binary_search_iterative,
-        binary_search_recursive
-    ]
-
-    for mega_list in list_of_lists:
-        for item in mega_list:
-            item.sort()
-
-    for func in list_of_functions:
-
-        for mega_list in list_of_lists:
-            search_time = datetime.timedelta(0,0,0)
-            for item in mega_list:
-                start = dt.now()
-                func(item)
-                search_time += dt.now() - start
-
-            print('{}, {} from {}'.format(func.__name__, search_time.total_seconds()/float(len(mega_list)),len(mega_list)))
+    print(binary_search_iterative([2, 12, 45, 65, 78, 89, 92, 95, 98, 112], 68))
+    # lists500 = []
+    # lists1000 = []
+    # lists10000 = []
+    # list_of_lists = [
+    #     lists500,
+    #     lists1000,
+    #     lists10000
+    # ]
+    #
+    # for _ in range(100):
+    #     lists500.append([random.randint(0, 1285398243) for _b in range(500)])
+    #
+    #     lists1000.append([random.randint(0, 1285398243) for _b in range(500)])
+    #
+    #     lists10000.append([random.randint(0, 1285398243) for _b in range(500)])
+    #
+    # for mega_list in list_of_lists:
+    #     search_time = datetime.timedelta(0, 0, 0)
+    #     for item in mega_list:
+    #         start = dt.now()
+    #         sequential_search(item)
+    #         search_time += dt.now() - start
+    #
+    #     print(
+    #     '{}, {} from {}'.format(sequential_search.__name__, search_time.total_seconds() / float(len(mega_list)), len(mega_list)))
+    #
+    # list_of_functions = [
+    #     ordered_sequential_search,
+    #     binary_search_iterative,
+    #     binary_search_recursive
+    # ]
+    #
+    # for mega_list in list_of_lists:
+    #     for item in mega_list:
+    #         item.sort()
+    #
+    # for func in list_of_functions:
+    #
+    #     for mega_list in list_of_lists:
+    #         search_time = datetime.timedelta(0,0,0)
+    #         for item in mega_list:
+    #             start = dt.now()
+    #             func(item)
+    #             search_time += dt.now() - start
+    #
+    #         print('{}, {} from {}'.format(func.__name__, search_time.total_seconds()/float(len(mega_list)),len(mega_list)))
