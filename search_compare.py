4b773a35a28f041e56db4c30cebef7cf8ad5f56e
staceyvo <staceyvo@gmail.com>
2018-03-17 15:00:50 -0400

    worked on Assignment 4

--- search_compare.py
+++ search_compare.py
@@ -3,21 +3,21 @@ import datetime
 from datetime import datetime as dt
 
 
-def sequential_search(search_list):
+def sequential_search(search_list, term=-1):
     for index in range(len(search_list)):
         if search_list[index] == -1:
             return index
     return None
 
 
-def ordered_sequential_search(search_list):
+def ordered_sequential_search(search_list, term=-1):
     for index in range(len(search_list)):
         if search_list[index] == -1:
             return index
     return None
 
 
-def binary_search_iterative(search_list, elem):
+def binary_search_iterative(search_list, elem=-1):
     left_bound = 0
     right_bound = len(search_list) - 1
     mid = (left_bound + right_bound) / 2
@@ -29,62 +29,76 @@ def binary_search_iterative(search_list, elem):
         elif elem > search_list[mid]:
             left_bound = mid + 1
         else:
-            right_bound = mid - 1
+            right_bound = mid
         mid = (left_bound + right_bound) / 2
     return None
 
 
 
 
-def binary_search_recursive(search_list):
-    pass
+def binary_search_recursive(search_list, term=-1):
+    if len(search_list) == 0:
+        return None
+
+    mid = len(search_list)/2
+    if search_list[mid] == term:
+        return mid
+    elif search_list[mid] > term:
+            return binary_search_recursive(search_list[:mid], term)
+    else:
+        result = binary_search_recursive(search_list[mid + 1:], term)
+        if result:
+            return result + mid
+        else:
+            return result
+
+
 
 
 if __name__ == '__main__':
-    print(binary_search_iterative([2, 12, 45, 65, 78, 89, 92, 95, 98, 112], 68))
-    # lists500 = []
-    # lists1000 = []
-    # lists10000 = []
-    # list_of_lists = [
-    #     lists500,
-    #     lists1000,
-    #     lists10000
-    # ]
-    #
-    # for _ in range(100):
-    #     lists500.append([random.randint(0, 1285398243) for _b in range(500)])
-    #
-    #     lists1000.append([random.randint(0, 1285398243) for _b in range(500)])
-    #
-    #     lists10000.append([random.randint(0, 1285398243) for _b in range(500)])
-    #
-    # for mega_list in list_of_lists:
-    #     search_time = datetime.timedelta(0, 0, 0)
-    #     for item in mega_list:
-    #         start = dt.now()
-    #         sequential_search(item)
-    #         search_time += dt.now() - start
-    #
-    #     print(
-    #     '{}, {} from {}'.format(sequential_search.__name__, search_time.total_seconds() / float(len(mega_list)), len(mega_list)))
-    #
-    # list_of_functions = [
-    #     ordered_sequential_search,
-    #     binary_search_iterative,
-    #     binary_search_recursive
-    # ]
-    #
-    # for mega_list in list_of_lists:
-    #     for item in mega_list:
-    #         item.sort()
-    #
-    # for func in list_of_functions:
-    #
-    #     for mega_list in list_of_lists:
-    #         search_time = datetime.timedelta(0,0,0)
-    #         for item in mega_list:
-    #             start = dt.now()
-    #             func(item)
-    #             search_time += dt.now() - start
-    #
-    #         print('{}, {} from {}'.format(func.__name__, search_time.total_seconds()/float(len(mega_list)),len(mega_list)))
+    lists500 = []
+    lists1000 = []
+    lists10000 = []
+    list_of_lists = [
+        lists500,
+        lists1000,
+        lists10000
+    ]
+#create 100 lists for 500, 1000, 10000
+    for _ in range(100):
+        lists500.append([random.randint(0, 1285398243) for _b in range(500)])
+
+        lists1000.append([random.randint(0, 1285398243) for _b in range(1000)])
+
+        lists10000.append([random.randint(0, 1285398243) for _b in range(10000)])
+
+    for mega_list in list_of_lists:
+        search_time = datetime.timedelta(0, 0, 0)
+        for item in mega_list:
+            start = dt.now()
+            sequential_search(item)
+            search_time += dt.now() - start
+
+        print(
+        '{}, {} from {} numbers'.format(sequential_search.__name__, search_time.total_seconds() / float(len(mega_list)), len(mega_list[0])))
+
+    list_of_functions = [
+        ordered_sequential_search,
+        binary_search_iterative,
+        binary_search_recursive
+    ]
+
+    for mega_list in list_of_lists:
+        for item in mega_list:
+            item.sort()
+
+    for func in list_of_functions:
+
+        for mega_list in list_of_lists:
+            search_time = datetime.timedelta(0,0,0)
+            for item in mega_list:
+                start = dt.now()
+                func(item)
+                search_time += dt.now() - start
+
+            print('{}, {} from {}'.format(func.__name__, search_time.total_seconds()/float(len(mega_list)),len(mega_list[0])))
--- sort_compare.py
+++ sort_compare.py
@@ -1,13 +1,68 @@
+import datetime
+import random
 
 
-def insertion_sort:
-    raise Exception('Not implemented yet')
+def insertion_sort(sort_list):
+    for index in range(len(sort_list)):
+        for left in range(index):
+            if sort_list[index] < sort_list[left]:
+                current = sort_list[index]
+                sort_list[index] = sort_list[left]
+                sort_list[left] = current
 
 
-def shell_sort:
-    raise Exception('Not implemented yet')
 
 
-if __name__ == '__name__':
-    insertion_sort()
-    shell_sort()
+def shell_sort(sort_list):
+    gap = len(sort_list) / 2
+    #creates multiple passes through list
+    while gap > 0:
+        index = 0
+        #scan from left to right minus our gap
+        while index + gap < len(sort_list):
+            if sort_list[index + gap] < sort_list[index]:
+                current = sort_list[index + gap]
+                sort_list[index + gap] = sort_list[index]
+                sort_list[index] = current
+
+                #scans from right to left plus our gap
+                right_index = index
+                while right_index - gap > 0 and sort_list[right_index] < sort_list[right_index - gap]:
+                    current = sort_list[right_index - gap]
+                    sort_list[right_index - gap] = sort_list[right_index]
+                    sort_list[right_index] = current
+                    right_index - gap
+
+            index += 1
+        gap /= 2
+
+
+
+def python_sort(sort_list):
+    sort_list.sort()
+
+
+if __name__ == '__main__':
+
+
+    # ins_sort = [random.randint(0, 100) for _ in range(25)]
+    # print(ins_sort)
+    # start = datetime.datetime.now()
+    # insertion_sort(ins_sort)
+    # duration = datetime.datetime.now() - start
+    # print(ins_sort)
+    # print(duration)
+    # print('')
+
+
+
+    for func in [insertion_sort, shell_sort, python_sort]:
+        small = [random.randint(0, 1e7) for _ in range(500)]
+        medium = [random.randint(0, 1e7) for _ in range(1000)]
+        large = [random.randint(0, 1e7) for _ in range(10000)]
+        for sort_list in [small, medium, large]:
+            start = datetime.datetime.now()
+            func(sort_list)
+            duration = datetime.datetime.now() - start
+            print('{}: {}'.format(func.__name__, duration))
+        print('')
